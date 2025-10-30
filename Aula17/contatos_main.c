a60a82342e3529d56b856622793114e2efbe95b2
RaimundoValter <valter.costa@ifce.edu.br>
2025-10-30 15:02:56 -0300

    Aual 18: Listas heterogêneas - Parte 1

--- Aula17/contatos_main.c
+++ Aula17/contatos_main.c
@@ -3,9 +3,9 @@
 int main(){
     Lista* casamento = lst_cria();
 
-    casamento = lst_insere(casamento, "Kauã", "88999880099");
-    casamento = lst_insere(casamento, "Vitor", "88911880044");
-    casamento = lst_insere(casamento, "Francisco", "88978880033");
+    casamento = lst_insere_ordenado(casamento, "Kauã", "88999880099");
+    casamento = lst_insere_ordenado(casamento, "Vitor", "88911880044");
+    casamento = lst_insere_ordenado(casamento, "Francisco", "88978880033");
 
     lst_imprime(casamento);
     
